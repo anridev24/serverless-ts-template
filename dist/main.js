82dd5166d08fd05f8f021d823c09baf666d16d17
Andronik Nazaryan <32931256+anridev24@users.noreply.github.com>
2022-01-20 18:04:09 +0400

    Delete dist directory

--- dist/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.start = void 0;
-const start = async function () {
-    console.log("Hello from TS");
-};
-exports.start = start;
-//# sourceMappingURL=main.js.map
